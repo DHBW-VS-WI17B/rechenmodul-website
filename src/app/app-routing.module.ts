5eea07bb1f97ce505465847ca0f346ec2b3688ca
Robin Genz <mail@robingenz.de>
2020-01-28 22:45:46 +0100

    refactor: fix tslint warnings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: '', loadChildren: () => import('@app/modules/dashboard/dashboard.module').then(m => m.DashboardModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
