a0b443462007efc8e5d14853492e4a85c8efda71
Robin Genz <mail@robingenz.de>
2020-01-28 23:07:46 +0100

    feat: set the basic dashboard style

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '@app/shared';
 import { ContingencyTableComponent, DashboardComponent, PointSequenceComponent } from './components';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, DashboardRoutingModule],
+    imports: [CommonModule, FormsModule, DashboardRoutingModule, SharedModule],
     declarations: [ContingencyTableComponent, DashboardComponent, PointSequenceComponent],
 })
 export class DashboardModule {}
